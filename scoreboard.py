5c22b06a4d851443904c0066acf4a404dfbd94c6
Jay2995 <josey02995@gmail.com>
2023-11-17 19:56:29 +0300

    added save state with data file

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,13 @@
 from turtle import Turtle;
+open 
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__();
         self.score = 0;
-        self.highs_score = 0;
+        with open("data.txt") as data:
+            self.highs_score = int(data.read());
         self.ht()
         self.goto(x=0, y=250)
         self.color("White");
@@ -18,6 +20,9 @@ class Scoreboard(Turtle):
     def reset_scoreboard(self):
         if self.score > self.highs_score:
             self.highs_score = self.score;
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highs_score}");
+
         self.score = 0;
         self.update_score(); 
 
